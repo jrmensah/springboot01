ce0d06f2fe261ca3776c8f903dd1148a0d7f6556
RVSW317004DT <RVSW317004DT@MC.MONTGOMERYCOLLEGE.EDU>
2017-08-21 11:26:43 -0400

    First commit- Hello World!

---
+++ src/main/java/me/jerilynmensah/springboot01/HomeController.java
@@ -0,0 +1,14 @@
+package me.jerilynmensah.springboot01;
+
+import org.springframework.stereotype.Controller;
+import org. springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homepage(){
+        return "index";
+    }
+}
